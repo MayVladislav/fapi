270397ee358cf85b9ca304e16b33c6ff1a6ba5ef
Vladislav <89187635177izi@gmail.com>
2024-08-20 15:36:20 +0300

    new update 1

---
+++ core/config.py
@@ -0,0 +1,11 @@
+from os import getenv
+from pydantic_settings import BaseSettings
+
+
+class Setting(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+    db_url: str = "sqlite+aiosqlite:///./db.sqlite3"
+    db_echo: bool = True
+
+
+settings = Setting()
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from typing import Annotated
 from fastapi import Path
 from items_views import router as items_router
 from users.views import router as user_router
+from core.models import Base, db_helper
+from api_v1 import router as router_v1
+from core.config import settings
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(items_router)
 app.include_router(user_router)
+app.include_router(router_v1, prefix=settings.api_v1_prefix, tags=["products"])
 
 
 @app.get("/")
