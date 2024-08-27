cd1d260ffb329513e7473f573b7be60b28447e3a
Vladislav <89187635177izi@gmail.com>
2024-08-27 11:48:47 +0300

    create relationship

--- core/models/post.py
+++ core/models/post.py
@@ -1,7 +1,10 @@
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import String, Text, ForeignKey
-
+from typing import TYPE_CHECKING
 from core.models import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Post(Base):
@@ -10,3 +13,4 @@ class Post(Base):
     title: Mapped[str] = mapped_column(String(100), unique=True)
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="posts")
--- core/models/user.py
+++ core/models/user.py
@@ -1,13 +1,17 @@
 from typing import Annotated
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import String
 from annotated_types import MinLen, MaxLen
 from pydantic import EmailStr
-
+from typing import TYPE_CHECKING
 from core.models import Base
+
+if TYPE_CHECKING:
+    from .post import Post
 
 
 class User(Base):
     __tablename__ = "user"
 
     username: Mapped[str] = mapped_column(String(32), unique=True)
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
